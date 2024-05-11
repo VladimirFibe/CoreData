200f361056adbdf2e4f7890613aad53fb0c65d0a
Vladimir Fibe <motiw@icloud.com>
2024-05-11 09:17:51 +0500

    apstract entities

---
+++ Cinema/Cinema/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ViewController: UIViewController {
+    let button = UIButton(type: .system)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
